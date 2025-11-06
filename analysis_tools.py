1e20ce2b796cf904aed72e9d562dad4ac023a94d
Antoine GROSSE <antoine.grosse@ntnu.no>
2025-11-06 13:20:04 +0100

    Updated analysis files

--- analysis_tools.py
+++ analysis_tools.py
@@ -3,18 +3,12 @@ import scipy.signal as sg
 import matplotlib.pyplot as plt
 import h5py
 
-def PQ_analysis(t,x):
-    peak_idxs = sg.find_peaks(x, prominence=0.001)[0]
-    T_n = np.diff(t[peak_idxs][1:-3]).mean()
-    # plt.sca(ax.flatten('F')[i_dof-1])
-    # plt.plot(t, x, label=label + f' ($T_n$={T_n:.2f})', color=color, linestyle=style)
-    # plt.plot(t[peak_idxs], x[peak_idxs], 'o', color=color, markersize=3)
-    pks = x[peak_idxs]
-    dphi = -np.diff(pks)
-    mean_amp = 0.5*(pks[1:]+pks[:-1])
+def PQ_analysis(x_pks):
+    dphi = -np.diff(x_pks)
+    mean_amp = 0.5*(x_pks[1:]+x_pks[:-1])
     y = np.divide(dphi,mean_amp)
     P,Q = np.polynomial.polynomial.polyfit(mean_amp,y,1)
-    return P, Q
+    return P, Q, mean_amp, y
 
 def bw_filter(data, cutoff, fs, order, switch):
     nyq = 0.5 * fs
@@ -120,3 +114,49 @@ def PSD_wave5(timeserie, dt):
     Sp = bw_filter(Sp, cutoff=0.1/f_fft[1], fs=1/f_fft[1], order=2, switch='low')
     f, Sp = f_fft[:NFFT//2], Sp[:NFFT//2]
     return f_fft[:NFFT//2], Sp[:NFFT//2]
+
+def compare_decay_tests(H_data, outputs, focus ,colors={},styles={},labels={},figsize=(16,7)):
+    H_data = H_data.selection(
+        {
+            'model':focus['model'],
+            'condition':focus['condition'],
+            'analysis':focus['analysis']
+        }
+    )
+    n_o = len(outputs)
+    fig,ax = plt.subplots(n_o // 2 + n_o % 2, 2, figsize=figsize, dpi=600)
+    legend_labels = []
+    res = {}
+    model = focus['model']
+    for cond in H_data.conds:
+        color = colors[cond] if len(colors) > 0 else None
+        style = styles[cond] if len(styles) > 0 else '-'
+        label = labels[cond] if len(labels) > 0 else cond
+        legend_labels.append(label)
+        dict_res = {}
+        for i_dof, output in enumerate(outputs):
+            t,x = H_data.timeserie({'model':model, 'condition':cond, 'analysis':'Dynamic'}, output, show=False)
+            peak_idxs = sg.find_peaks(x, prominence=0.001)[0]
+            T_n = np.diff(t[peak_idxs][1:-1]).mean()
+            plt.sca(ax.flatten('F')[i_dof])
+            plt.plot(t, x, label=label + f' ($T_n$={T_n:.2f})', color=color, linestyle=style)
+            plt.plot(t[peak_idxs], x.values[peak_idxs], 'o', color=color, markersize=3)
+
+            if output == focus['output']:   
+                x_filt = x.values-np.mean(x.values)
+                plt.plot(t, x_filt, color=color, alpha=0.3, linestyle=style)
+                plt.plot(t[peak_idxs], x_filt[peak_idxs], 'o', color=color, alpha=0.3, markersize=3)
+                P,Q, amp, y = PQ_analysis(x_filt[peak_idxs])
+                print(output + ' analysis for cond ' + cond + ' : P = ' + str(P) + ' ; Q = ' + str(Q) + ' ; T_n = ' + str(T_n))
+                dict_res[output] = (P,Q,amp,y,T_n)
+            
+            plt.xlabel('Time (s)')
+            plt.ylabel(f'{output}')
+
+        res[cond] = dict_res
+    for axis in ax.flatten('F'):
+        axis.grid()
+        axis.legend(loc='upper right', fontsize='small')
+    # fig.legend(legend_labels, loc='upper center', fontsize='small',ncols=len(legend_labels), bbox_to_anchor=(0.5, 1.05))
+    fig.tight_layout()
+    return fig, ax, res
